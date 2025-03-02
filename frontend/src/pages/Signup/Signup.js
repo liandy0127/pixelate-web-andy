852b694f1fa3aa5fcffb4a4401a77dd5a0789a20
Andy Li <sl2535@cornell.edu>
2025-03-01 21:02:27 -0500

    fix issues

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -4,10 +4,10 @@ import "./Signup.css";
 const Signup = () => {
   const COGNITO_DOMAIN = process.env.REACT_APP_COGNITO_DOMAIN;
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const REDIRECT_URI = encodeURIComponent('https://pixelateinc.com/dashboard');
+  const REDIRECT_URI = `https%3A%2F%2Fpixelateinc.com%2Fdashboard`;
 
 
-  const signupUrl = `https://${COGNITO_DOMAIN}/login/continue?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}response_type=code&scope=email+openid+profile`;
+  const signupUrl = `https://${COGNITO_DOMAIN}/login/continue?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email+openid+profile`;
 
   return (
     <div className="signup-container">
