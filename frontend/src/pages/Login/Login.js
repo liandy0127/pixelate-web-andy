de5adb258d2bbe67539ba279fbb182ad233ea74f
Andy Li <sl2535@cornell.edu>
2025-03-01 21:04:41 -0500

    Add www.

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -5,7 +5,7 @@ import "./Login.css";
 const Login = () => {
   const COGNITO_DOMAIN = process.env.REACT_APP_COGNITO_DOMAIN;
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const REDIRECT_URI = `https%3A%2F%2Fpixelateinc.com%2Fdashboard`;
+  const REDIRECT_URI = `https%3A%2F%2Fwww.pixelateinc.com%2Fdashboard`;
 
   const loginUrl = `https://${COGNITO_DOMAIN}/login/continue?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email+openid+profile`;
 
