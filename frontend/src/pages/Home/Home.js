42d216eab667c103d8f052c8a01e9d63eb569dd9
Andy Li <sl2535@cornell.edu>
2025-03-02 19:57:12 -0500

    delete original navbar

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -31,7 +31,8 @@ const Home = () => {
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#services">Services</a></li>
-              <li><a href="/team">Team</a></li>
+              <li><a href="#team">Team</a></li>
+              <li><a href="login">Login</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
@@ -103,6 +104,11 @@ const Home = () => {
           /> */}
         </div>
       </section>
+
+
+
+
+
 
       {/* CONTACT SECTION */}
       <section className="contact-section" id="contact">
